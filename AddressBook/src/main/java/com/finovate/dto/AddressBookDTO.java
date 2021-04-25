e84f369d35484f0aa3706d5735c1ee2ad61b1f57
Bhushan Patil <bhupatil001@gmail.com>
2021-04-25 08:05:32 +0530

    Added: demonstrate the various HTTP methods and URL test using curl and postman

---
+++ AddressBook/src/main/java/com/finovate/dto/AddressBookDTO.java
@@ -0,0 +1,18 @@
+package com.finovate.dto;
+
+public class AddressBookDTO {
+
+	public String firstName;
+	public String lastName;
+
+	public AddressBookDTO(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBookDTO [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+}
