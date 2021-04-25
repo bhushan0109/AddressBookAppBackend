168ae0c9d3ca879f3cc5da92f835528cb6d4345c
Bhushan Patil <bhupatil001@gmail.com>
2021-04-25 11:36:16 +0530

    Started: use lambok library for logging

--- AddressBook/src/main/java/com/finovate/AddressBookApplication.java
+++ AddressBook/src/main/java/com/finovate/AddressBookApplication.java
@@ -2,12 +2,17 @@ package com.finovate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Address Book app started in {} environment..", context.getEnvironment().getProperty("environment"));
 	}
 
 }
