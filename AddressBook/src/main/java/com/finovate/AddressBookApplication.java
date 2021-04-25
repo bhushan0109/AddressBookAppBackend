63c6cc94f76eb04a1d9c1da7a4473acea0576e72
Bhushan Patil <bhupatil001@gmail.com>
2021-04-25 14:26:15 +0530

    Added: Database setting in application.properties

--- AddressBook/src/main/java/com/finovate/AddressBookApplication.java
+++ AddressBook/src/main/java/com/finovate/AddressBookApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
-
-@SpringBootApplication
 @Slf4j
+@SpringBootApplication
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
 		log.info("Address Book app started in {} environment..", context.getEnvironment().getProperty("environment"));
+		log.info("ADDRESS BOOK DB User is = {} ",context.getEnvironment().getProperty("spring.datasource.username"));
+		log.info("ADDRESS BOOK DB password is = {} ",context.getEnvironment().getProperty("spring.datasource.password"));
 	}
 
 }
